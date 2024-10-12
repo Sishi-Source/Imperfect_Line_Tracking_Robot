ebaaeb39ecdda8c76db62ad196df6a70fd0f59bc
Sishi-Source <sidney.manzano.56@gmail.com>
2024-10-12 11:16:27 +0800

    added motor speed

--- src/main.c
+++ src/main.c
@@ -2,6 +2,7 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "driver/gpio.h"
+#include "driver/ledc.h"
 #include "esp_wifi.h"
 
 // Define GPIO pins for the IR sensors
@@ -10,10 +11,12 @@
 #define RIGHT_SENSOR_PIN  18
 
 // Define GPIO pins for motor control
-#define AIN1  25 // Motor A direction
-#define AIN2  26 // Motor A reverse
-#define BIN1  27 // Motor B direction
-#define BIN2  14 // Motor B reverse
+#define PWMA 32 // Motor A speed
+#define AIN1 25 // Motor A direction
+#define AIN2 33 // Motor A reverse
+#define PWMB 14 // Motor B speed
+#define BIN1 26 // Motor B direction
+#define BIN2 27 // Motor B reverse
 
 // Define threshold for line detection
 #define LINE_DETECTED 0 // 0 means line detected (LOW output from sensor)
@@ -25,10 +28,17 @@ void turn_right();
 void stop_motors();
 void disable_bt();
 void disable_wifi();
+void init_pwm_channel(int gpio, int channel);
+void set_speed(int channel, int speed);
 
 void app_main() {
   init_gpio();
   disable_wifi();
+
+  init_pwm_channel(PWMA, LEDC_CHANNEL_0);
+  init_pwm_channel(PWMB, LEDC_CHANNEL_0);
+
+  set_speed(LEDC_CHANNEL_0, 50);
 
   while (1) {
     int left_sensor = gpio_get_level(LEFT_SENSOR_PIN);
@@ -95,4 +105,22 @@ void disable_wifi() {
   esp_wifi_stop();
   esp_wifi_disconnect();
   esp_wifi_deinit();
+}
+
+void init_pwm_channel(int gpio, int channel) {
+  ledc_channel_config_t pwm_config = {
+    .channel = channel,
+    .duty = 0,
+    .gpio_num = gpio,
+    .speed_mode = LEDC_HIGH_SPEED_MODE,
+    .hpoint = 0,
+    .timer_sel = LEDC_TIMER_0
+  };
+  ledc_channel_config(&pwm_config);
+}
+
+void set_speed(int channel, int speed) {
+  int duty_cycle = (speed * 8191)/100;
+  ledc_set_duty(LEDC_HIGH_SPEED_MODE, channel, duty_cycle);
+  ledc_update_duty(LEDC_HIGH_SPEED_MODE, channel);
 }
